101f4570d456ace988dc8e5e1290356a7a59f5d8
Christian Bartels <c.bartels@bequadrat.de>
2015-02-08 12:39:30 +0100

    Update templateselect.php
    Cleaning things a bit up

--- core/components/templateselect/elements/plugins/templateselect.php
+++ core/components/templateselect/elements/plugins/templateselect.php
@@ -1,18 +1,6 @@
 <?php
-/**
- * templateselect
- *
- * @package templateselect
- * @author bequadrat
- *
- */
 
 $modx->lexicon->load('templateselect:default');
 $modx->regClientStartupScript(MODX_ASSETS_URL . 'components/templateselect/templateselect.js' );
 $modx->regClientCSS(MODX_ASSETS_URL . 'components/templateselect/templateselect.css' );
-$modx->regClientStartupHTMLBlock(
-    '<script type="text/javascript">
-        templateSelectPrompt="'.$modx->lexicon('choose_template').'";
-        templateSelectDefault='.$modx->getOption('default_template').';
-    </script>'
-);
+$modx->regClientStartupHTMLBlock( '<script type="text/javascript">' . PHP_EOL . 'templateSelectPrompt="' . $modx->lexicon('choose_template') . '";' . PHP_EOL . 'templateSelectDefault=' . $modx->getOption('default_template') . ';' . PHP_EOL . '</script>');
